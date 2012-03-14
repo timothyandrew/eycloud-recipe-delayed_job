95fdd10c3b19119293eb9f3067d1e7c79b71c480
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-14 15:41:05 -0700

    convert recipe into a gem

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name              "delayed_job"
+description       "Delayed Job for EY Cloud"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer        "Engine Yard LLC"
+maintainer_email  "drnic@engineyard.com"
+version           "1.0"
