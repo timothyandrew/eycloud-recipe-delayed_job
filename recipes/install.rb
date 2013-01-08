5be752d3c6396b4c16a7703f92977ecdecdd1c49
Timothy Andrew <mail@timothyandrew.net>
2013-01-08 14:32:01 +0530

    Install delayed_job on the app_master instance

--- recipes/install.rb
+++ recipes/install.rb
@@ -3,10 +3,8 @@
 # Recipe:: install
 #
 
-# This recipe will setup `delayed_job` on a Solo instance environment or on named Utility instances in a cluster environment. 
-# Name your Utility instances with prefixes: `dj`, `delayed_job`, `delayedjob`. For example, `dj1`, `delayedjob4`.
-if node[:instance_role] == "solo" || node[:instance_role] == "eylocal" || 
-        (node[:instance_role] == "util" && node[:name] =~ /^(dj|delayed_job|delayedjob)/)
+# This recipe will setup `delayed_job` on the app_master instance
+if node[:instance_role] == "app_master"
   delayed_job_applications().each do |app_name,data|
   
     directory "/engineyard/bin" do
